2998b3e29ad0573dc642b7a175db1d2523ed194c
abdullah11119 <trapstarfrance.store@gmail.com>
2026-02-05 21:26:13 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,275 +1,194 @@
 #!/usr/bin/env python3
-
-import json
-from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
-from telegram import ParseMode
-from telegram.ext.dispatcher import run_async
 import os
-import sys
-from upload import upload
-from creds import Creds
-from pySmartDL import SmartDL
-from pydrive.auth import GoogleAuth
-
-from plugins import TEXT
-
-from plugins.tok_rec import is_token
+import json
+import asyncio
 from time import time
 import subprocess
-from plugins.dpbox import DPBOX
-from plugins.wdl import wget_dl
-import re
+
+from pySmartDL import SmartDL
+from pydrive.auth import GoogleAuth
 from mega import Mega
 
+from upload import upload
+from creds import Creds
+from plugins import TEXT
+from plugins.tok_rec import is_token
+from plugins.dpbox import DPBOX
+from plugins.wdl import wget_dl
+
+from telegram import Update
+from telegram.constants import ParseMode
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+
 gauth = GoogleAuth()
-
-
-######################################################################################
-
 bot_token = Creds.TG_TOKEN
-updater = Updater(token=bot_token,  workers = 8 , use_context=True)
-dp = updater.dispatcher                                                          #
-#
+
+# Create the bot application
+app = ApplicationBuilder().token(bot_token).build()
 
 ######################################################################################
+# Async helper functions
+######################################################################################
 
-
-@run_async
-def help(update, context):
+async def send_msg(update: Update, text: str):
+    """Send message safely"""
     try:
-        context.bot.send_message(chat_id=update.message.chat_id,
-                                 text=TEXT.HELP, parse_mode=ParseMode.HTML)
+        await update.message.reply_text(text, parse_mode=ParseMode.HTML)
     except Exception as e:
-        print(e)
-# command ```auth```
-@run_async
-def auth(update, context):
-    FOLDER_MIME_TYPE = 'application/vnd.google-apps.folder'
-    drive: GoogleDrive
-    http = None
-    initial_folder = None
-    ID = update.message.from_user.id
-    ID = str(ID)
+        print("Send message error:", e)
+
+######################################################################################
+# Command Handlers
+######################################################################################
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await send_msg(update, TEXT.START.format(update.message.from_user.first_name))
+
+async def help_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await send_msg(update, TEXT.HELP)
+
+async def status(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await send_msg(update, TEXT.UPDATE)
+
+async def auth(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    ID = str(update.message.from_user.id)
     try:
         gauth.LoadCredentialsFile(ID)
     except Exception as e:
-        print("Cred file missing :", e)
+        print("Cred file missing:", e)
 
     if gauth.credentials is None:
         authurl = gauth.GetAuthUrl()
-
         AUTH = TEXT.AUTH_URL.format(authurl)
-        context.bot.send_message(
-            chat_id=update.message.chat_id, text=AUTH, parse_mode=ParseMode.HTML)
-
+        await send_msg(update, AUTH)
     elif gauth.access_token_expired:
-        # Refresh Token if expired
         gauth.Refresh()
     else:
-        # auth with  saved creds
         gauth.Authorize()
-        context.bot.send_message(
-            chat_id=update.message.chat_id, text=TEXT.ALREADY_AUTH)
+        await send_msg(update, TEXT.ALREADY_AUTH)
 
-
-# It will handle Sent Token By Users
-@run_async
-def token(update, context):
+async def token(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = update.message.text
-    ID = update.message.from_user.id
-    ID = str(ID)
+    ID = str(update.message.from_user.id)
 
     if is_token(msg):
-        token = msg.split()[-1]
-        print(token)
+        token_val = msg.split()[-1]
+        print(token_val)
         try:
-            gauth.Auth(token)
+            gauth.Auth(token_val)
             gauth.SaveCredentialsFile(ID)
-            context.bot.send_message(
-                chat_id=update.message.chat_id, text=TEXT.AUTH_SUCC)
+            await send_msg(update, TEXT.AUTH_SUCC)
         except Exception as e:
-            print("Auth Error :", e)
-            context.bot.send_message(chat_id=update.message.chat_id,
-                                     text=TEXT.AUTH_ERROR)
-   
+            print("Auth Error:", e)
+            await send_msg(update, TEXT.AUTH_ERROR)
 
-# command `Start`
-@run_async
-def start(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id,
-                             text=TEXT.START.format(update.message.from_user.first_name), parse_mode=ParseMode.HTML)
-
-# command `revoke`
-@run_async
-def revoke_tok(update, context):
-    ID = update.message.chat_id
-    ID = str(ID)
+async def revoke_tok(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    ID = str(update.message.chat_id)
     try:
         os.remove(ID)
-        context.bot.send_message(
-            chat_id=update.message.chat_id, text=TEXT.REVOKE_TOK)
+        await send_msg(update, TEXT.REVOKE_TOK)
     except Exception as e:
         print(e)
-        context.bot.send_message(
-            chat_id=update.message.chat_id, text=TEXT.REVOKE_FAIL)
+        await send_msg(update, TEXT.REVOKE_FAIL)
 
-# It will Handle Sent Url
-@run_async
-def UPLOAD(update, context):
+######################################################################################
+# Upload handler
+######################################################################################
 
-    url = update.message.text
-    url = url.split()[-1]
-    sent_message = context.bot.send_message(
-        chat_id=update.message.chat_id, text=TEXT.PROCESSING)
+async def UPLOAD(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    url = update.message.text.split()[-1]
+    sent_message = await update.message.reply_text(TEXT.PROCESSING)
 
-    ID = update.message.chat_id
-    ID = str(ID)
-    os.path.isfile(ID)
-    if os.path.isfile(ID):
-        # Openlaod Stuffs
+    ID = str(update.message.chat_id)
+    if not os.path.isfile(ID):
+        await send_msg(update, TEXT.NOT_AUTH)
+        return
 
-        # I will Add This Later
+    DownloadStatus = False
+
+    try:
         if "openload" in url or "oload" in url:
-            
-            DownloadStatus = False
-            sent_message.edit_text("Openload No longer avalible")
+            await sent_message.edit_text("Openload No longer available")
             return
-        
-            # Here is DropBox Stuffs
-        elif 'dropbox.com' in url:
 
+        elif 'dropbox.com' in url:
             url = DPBOX(url)
             filename = url.split("/")[-1]
-            print("Dropbox link Downloading Started : {}".format(
-                url.split("/")[-1]))
-            sent_message.edit_text(TEXT.DP_DOWNLOAD)
-            # filename = wget.download(url)
+            print("Dropbox downloading started:", filename)
+            await sent_message.edit_text(TEXT.DP_DOWNLOAD)
             filename = wget_dl(str(url))
-            print("Downloading Complete : {}".format(filename))
-            sent_message.edit_text(TEXT.DOWN_COMPLETE)
+            await sent_message.edit_text(TEXT.DOWN_COMPLETE)
             DownloadStatus = True
-           # Here IS Mega Links stuffs
-        elif 'mega.nz' in url:
 
+        elif 'mega.nz' in url:
             try:
-                print("Downlaoding Started")
-                sent_message.edit_text(TEXT.DOWN_MEGA)
+                await sent_message.edit_text(TEXT.DOWN_MEGA)
                 m = Mega.from_credentials(TEXT.MEGA_EMAIL, TEXT.MEGA_PASSWORD)
                 filename = m.download_from_url(url)
-                print("Downloading Complete Mega :", filename)
-                sent_message.edit_text(TEXT.DOWN_COMPLETE)
-
+                print("Mega download complete:", filename)
+                await sent_message.edit_text(TEXT.DOWN_COMPLETE)
                 DownloadStatus = True
             except Exception as e:
-                print("Mega Downloding Error :", e)
-                sent_message.edit_text("Mega Downloading Error !!")
+                print("Mega downloading error:", e)
+                await sent_message.edit_text("Mega downloading error!!")
 
         else:
             try:
                 filename = url.split("/")[-1]
-
-                print("Downloading Started : {}".format(url.split("/")[-1]))
-                sent_message.edit_text(TEXT.DOWNLOAD)
-                # filename = wget.download(url)
+                await sent_message.edit_text(TEXT.DOWNLOAD)
                 filename = wget_dl(str(url))
-                print("Downloading Complete : {}".format(filename))
-                sent_message.edit_text(TEXT.DOWN_COMPLETE)
+                await sent_message.edit_text(TEXT.DOWN_COMPLETE)
                 DownloadStatus = True
-
             except Exception as e:
-                # switch To second download(SmartDl Downloader) `You can activate it throungh TEXT file`
+                print(e)
                 if TEXT.DOWN_TWO:
-                    print(TEXT.DOWN_TWO)
-                    try:
-                        sent_message.edit_text(
-                            "Downloader 1 Error:{} \n\n Downloader 2 :Downloading Started...".format(e))
-
-                        obj = SmartDL(url)
-                        obj.start()
-                        filename = obj.get_dest()
-                        DownloadStatus = True
-                    except Exception as e:
-                        print(e)
-                        sent_message.edit_text(
-                            "Downloading error :{}".format(e))
-                        DownloadStatus = False
+                    await sent_message.edit_text(
+                        f"Downloader1 error: {e}\nDownloader2 starting..."
+                    )
+                    obj = SmartDL(url)
+                    obj.start()
+                    filename = obj.get_dest()
+                    DownloadStatus = True
                 else:
-                    print(e)
-                    sent_message.edit_text("Downloading error :{}".format(e))
+                    await sent_message.edit_text(f"Downloading error: {e}")
                     DownloadStatus = False
 
-            # Checking Error Filename
-        if "error" in filename:
-                # print(filename)
-                # print(filename[0],filename[-1],filename[1])
-            sent_message.edit_text("Downloading Error !! ")
-            os.remove(filename[-1])
-
-            ##########Uploading part  ###################
-        try:
-
-            if DownloadStatus:
-                sent_message.edit_text(TEXT.UPLOADING)
-
-                SIZE = (os.path.getsize(filename))/1048576
-                SIZE = round(SIZE)
-                FILENAME = filename.split("/")[-1]
-                try:
-                    FILELINK = upload(filename, update,
-                                      context, TEXT.drive_folder_name)
-                except Exception as e:
-                    print("error Code : UPX11", e)
-                    sent_message.edit_text("Uploading fail :{}".format(e))
-                else:
-                    sent_message.edit_text(TEXT.DOWNLOAD_URL.format(
-                        FILENAME, SIZE, FILELINK), parse_mode=ParseMode.HTML)
-                print(filename)
-                try:
-                    os.remove(filename)
-                except Exception as e:
-                    print(e)
-        except Exception as e:
-            print("Error code UXP12", e)
-            if DownloadStatus:
-                sent_message.edit_text("Uploading fail : {}".format(e))
-                try:
-                    os.remove(filename)
-                except Exception as e:
-                    print("Error code UXP13", e)
+        if DownloadStatus:
+            await sent_message.edit_text(TEXT.UPLOADING)
+            SIZE = round(os.path.getsize(filename)/1048576)
+            FILENAME = filename.split("/")[-1]
+            try:
+                FILELINK = upload(filename, update, context, TEXT.drive_folder_name)
+            except Exception as e:
+                print("Upload error:", e)
+                await sent_message.edit_text(f"Uploading fail: {e}")
             else:
-                sent_message.edit_text("Uploading fail :", e)
+                await sent_message.edit_text(TEXT.DOWNLOAD_URL.format(FILENAME, SIZE, FILELINK))
+            try:
+                os.remove(filename)
+            except:
+                pass
 
-    else:
-        context.bot.send_message(
-            chat_id=update.message.chat_id, text=TEXT.NOT_AUTH)
+    except Exception as e:
+        print("UPLOAD handler error:", e)
+        await sent_message.edit_text(f"Error: {e}")
 
+######################################################################################
+# Add handlers
+######################################################################################
 
-def status(update, context):
-    context.bot.send_message(
-        chat_id=update.message.chat_id, text=TEXT.UPDATE, parse_mode=ParseMode.HTML)
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("help", help_cmd))
+app.add_handler(CommandHandler("auth", auth))
+app.add_handler(CommandHandler("revoke", revoke_tok))
+app.add_handler(CommandHandler("update", status))
+app.add_handler(MessageHandler(filters.Regex(r"http"), UPLOAD))
+app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), token))
 
+######################################################################################
+# Run the bot
+######################################################################################
 
-update_status = CommandHandler('update', status)
-dp.add_handler(update_status)
-
-start_handler = CommandHandler('start', start)
-dp.add_handler(start_handler)
-
-downloader_handler = MessageHandler(Filters.regex(r'http'), UPLOAD)
-dp.add_handler(downloader_handler)
-
-help_handler = CommandHandler('help', help)
-dp.add_handler(help_handler)
-
-auth_handler = CommandHandler('auth', auth)
-dp.add_handler(auth_handler)
-
-token_handler = MessageHandler(Filters.text, token)
-dp.add_handler(token_handler)
-
-revoke_handler = CommandHandler('revoke', revoke_tok)
-dp.add_handler(revoke_handler)
-
-
-updater.start_polling()
-updater.idle()
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(app.run_polling())
